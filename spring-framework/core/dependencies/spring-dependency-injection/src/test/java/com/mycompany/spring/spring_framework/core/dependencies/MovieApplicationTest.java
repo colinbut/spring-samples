d02a2fe346c9760dc4787d3c64a1d63a28fe16e7
Colin But <colinbut@users.noreply.github.com>
2015-04-28 20:44:47 +0100

    test classes

---
+++ spring-framework/core/dependencies/spring-dependency-injection/src/test/java/com/mycompany/spring/spring_framework/core/dependencies/MovieApplicationTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.mycompany.spring.spring_framework.core.dependencies;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author colin
+ *
+ */
+public class MovieApplicationTest {
+
+	@Test
+	public void testBeansSetup(){
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		
+		int numberOfBeans = context.getBeanDefinitionCount();
+		String[] beanNames = context.getBeanDefinitionNames();
+		
+		System.out.println("Number of beans created: " + numberOfBeans);
+		for(String beanName : beanNames) {
+			System.out.println(beanName);
+		}
+		
+		System.out.println(context.getId());
+		System.out.println(context.getApplicationName());
+		System.out.println(context.getEnvironment().toString());
+		System.out.println(context.getDisplayName());
+		
+		
+		((ConfigurableApplicationContext)context).close();
+		
+	}
+}
---
+++ spring-framework/core/dependencies/spring-dependency-injection/src/test/java/com/mycompany/spring/spring_framework/core/dependencies/MovieFinderTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.mycompany.spring.spring_framework.core.dependencies;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+/**
+ * @author colin
+ *
+ */
+public class MovieFinderTest {
+
+	@Test
+	public void testMovieFinderSetup(){
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		MovieFinder movieFinder = context.getBean("movieFinder", MovieFinder.class);
+		
+		assertNotNull(movieFinder);
+		
+		((ConfigurableApplicationContext)context).close();
+		
+	}
+}
