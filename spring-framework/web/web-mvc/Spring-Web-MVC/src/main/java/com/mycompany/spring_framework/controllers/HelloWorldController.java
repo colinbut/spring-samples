cb373a3c5daf436865b07f77f1270cebe9b5fc67
Colin But <colinbut@users.noreply.github.com>
2015-06-02 21:23:08 +0100

    added servlet 3.1 support

--- spring-framework/web/web-mvc/Spring-Web-MVC/src/main/java/com/mycompany/spring_framework/controllers/HelloWorldController.java
+++ spring-framework/web/web-mvc/Spring-Web-MVC/src/main/java/com/mycompany/spring_framework/controllers/HelloWorldController.java
@@ -5,15 +5,17 @@ package com.mycompany.spring_framework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author colin
  *
  */
 @Controller
+@RequestMapping("/example")
 public class HelloWorldController {
 
-	@RequestMapping("/hello")
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
 	public String displayHelloWorld() {
 		return "Hello";
 	}
