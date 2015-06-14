25c03d371d9d124f8e965a9124ad4a54ff42076a
Colin But <colinbut@users.noreply.github.com>
2015-06-14 18:58:21 +0100

    Spring web MVC matrix variables

--- spring-framework/web/web-mvc/Spring-Web-MVC/src/main/java/com/mycompany/spring_framework/controllers/StudentDetailsController.java
+++ spring-framework/web/web-mvc/Spring-Web-MVC/src/main/java/com/mycompany/spring_framework/controllers/StudentDetailsController.java
@@ -8,11 +8,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mycompany.spring_framework.model.Semester;
 import com.mycompany.spring_framework.model.Student;
 import com.mycompany.spring_framework.model.UniversityClass;
 import com.mycompany.spring_framework.service.StudentService;
@@ -27,6 +28,13 @@ public class StudentDetailsController {
 	@Autowired
 	private StudentService studentService;
 	
+	/**
+	 * URI Template Patterns - Path Variable
+	 * 
+	 * @param studentId
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value ="/viewStudents/{studentId}", method=RequestMethod.GET)
 	public String viewStudentDetails(@PathVariable int studentId, Model model) {
 		Student student = studentService.findStudentByStudentId(studentId);
@@ -34,8 +42,14 @@ public class StudentDetailsController {
 		return "studentDetails";
 	}
 	
-	@RequestMapping(value="/viewStudents/{studentId}/classes/{className}", method=RequestMethod.GET, 
-			produces="application/json")
+	/**
+	 * URI template patterns multiple path variables
+	 * 
+	 * @param studentId
+	 * @param className
+	 * @return
+	 */
+	@RequestMapping(value="/viewStudents/{studentId}/classes/{className}", method=RequestMethod.GET)
 	public String viewClassDetails(@PathVariable int studentId, @PathVariable String className) {
 		
 		// get student
@@ -48,6 +62,36 @@ public class StudentDetailsController {
 			if (clazz.getName().equals(className) ) {
 				clasz = clazz;
 			}
+		}
+		
+		return "studentDetails";
+	}
+	
+	/**
+	 * Matrix Variables
+	 * 
+	 * e.g.
+	 * cars;color=red;year=2013
+	 * 
+	 * 
+	 * @param semester
+	 */
+	@RequestMapping(value = "/viewStudents/checkSemester/{semesterParam}/checkSemester/{semesterParam2}", 
+			method=RequestMethod.GET)
+	public String matrixVariables(
+			@PathVariable String semesterParam,
+			@PathVariable String semesterParam2,
+			@MatrixVariable(value="p", pathVar="semesterParam") int p1,
+			@MatrixVariable(value="p", pathVar="semesterParam2") int p2,
+			@MatrixVariable(required = false, defaultValue = "1") int q ) {
+		
+		System.out.println("Value of q: " + q);
+		System.out.println("Value of p1: " + p1);
+		System.out.println("Value of p2: " + p2);
+		
+		Semester semester = Semester.valueOf(semesterParam);
+		if(semester != null) {
+			System.out.println("Access data for supplied semester: " + semesterParam);
 		}
 		
 		return "studentDetails";
